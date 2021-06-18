6936c7758eee3f11d33f7eee28e617dfc2593a06
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-06-19 01:29:29 +0300

    lab08 fixing the branchs

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,70 +1,27 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react'
 
-export default class Movies extends Component {
-  state = {
-    locationData: "",
-    error: "",
-    moviesData: [],
-  };
-
-  getLocInfo = (e) => {
-    e.preventDefault();
-    let cityName = e.target.cityName.value;
-
-    let serverURL = process.env.REACT_APP_SERVER;
-    let url = `${serverURL}/weather2?searchQuery=${cityName}`;
-    let movieURL = `${serverURL}/movies?searchQuery=${cityName}`;
-
-    axios
-      .get(url)
-      .then((data) => {
-        this.setState({ locationData: data.data[0], error: "" });
-      })
-      .catch((error) => {
-        this.setState({ error: "There is an error" });
-      });
-
-    axios
-      .get(movieURL)
-      .then((data) => {
-        this.setState({ moviesData: data.data, error: "" });
-        console.log(this.state.moviesData);
-      })
-      .catch((error) => {
-        this.setState({ error: "There is an error" });
-      });
-  };
+export class Movies extends Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.getLocInfo}>
-          <input type="text" name="cityName" />
-          <input type="submit" name="submit" value="submit" />
-        </form>
-
-        {this.state.error ? (
-          <p>error: {this.state.error}</p>
-        ) : (
-          this.state.locationData && (
-            <>
-              <p>description : {this.state.locationData.description}</p>
-              <p>datetime : {this.state.locationData.datetime}</p>
-            </>
-          )
-        )}
-        <>
-          {this.state.moviesData.map((item) => {
-            return (
-              <figure>
-                <img src={item.img} />
-                <figcaption>{item.title}</figcaption>
-              </figure>
-            );
-          })}
-        </>
+      <div> {this.props.MovieData.map((movieObj) => {
+        return (
+          <div>
+            <img
+              className="map"
+              src={movieObj.img}
+              alt=""
+            />
+            <p className="">{movieObj.overview}</p>
+            <p className="">{movieObj.vote_average}</p>
+            <p className="">{movieObj.popularity}</p>
+            <p className="">{movieObj.title}</p>
+          </div>);
+      })}
       </div>
-    );
+
+    )
   }
 }
+
+export default Movies
